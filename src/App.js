bf03ea5c3763e4f3ae96968de2b9a40655b6bb78
Abdify <abdullah.fahimm08@gmail.com>
2022-03-14 23:27:57 +0600

    tailwind css and configs added, components added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar/Sidebar";
 import routes from "./routes";
 
 const App = () => (
     <Router>
-        <Routes>
-            {routes.map((route, index) => {
-                return (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        exact={route.exact}
-                        element={
-                            <route.layout>
-                                <route.component />
-                            </route.layout>
-                        }
-                    />
-                );
-            })}
-        </Routes>
+        <Sidebar />
+        <div className="relative md:ml-64 bg-blueGray-100 min-h-screen">
+            <Routes>
+                {routes.map((route, index) => {
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            exact={route.exact}
+                            element={
+                                <route.layout>
+                                    <route.component />
+                                </route.layout>
+                            }
+                        />
+                    );
+                })}
+            </Routes>
+        </div>
     </Router>
 );
 export default App;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+/*eslint-disable*/
+import React from "react";
+
+
+export default function Sidebar() {
+  return (
+    <nav>
+      This is the sidebar
+    </nav>
+  );
+}
